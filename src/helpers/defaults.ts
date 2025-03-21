26441eadd350dd77dc961a818e3bdab697afc080
Erik Schwarz <erik.schwarz@psi.ch>
2025-03-21 16:26:41 +0100

    Defined defaults in separate file

---
+++ src/helpers/defaults.ts
@@ -0,0 +1,20 @@
+export const defaultPlotBackgroundColor = "#fcfcfc";
+export const defaultXAxisGridColor = "#ebebeb";
+export const defaultYAxisGridColor = "#ebebeb";
+export const defaultCurveColors = [
+    "#1f77b4",
+    "#ff7f0e",
+    "#2ca02c",
+    "#d62728",
+    "#9467bd",
+    "#8c564b",
+    "#e377c2",
+    "#7f7f7f",
+    "#bcbd22",
+    "#17becf",
+];
+export const defaultYAxisScaling = "linear";
+export const defaultCurveShape = "linear";
+
+export const defaultWidgetWidth = 6;
+export const defaultWidgetHeight = 12;
