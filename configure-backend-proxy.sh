d98a5e33aac2cecc98c608a7d30b33fe2a921c1a
Erik Schwarz <erik.schwarz@psi.ch>
2025-05-16 09:50:56 +0200

    Switched variable susbstitution to use envsubst instead of sed

--- configure-backend-proxy.sh
+++ configure-backend-proxy.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-BACKEND_URL="${DATA_BOARD_INTERNAL_BACKEND_URL:-http://localhost:8000}"
+DATA_BOARD_INTERNAL_BACKEND_URL="${DATA_BOARD_INTERNAL_BACKEND_URL:-http://localhost:8000}"
 
-echo "Injecting backend URL: $BACKEND_URL"
+echo "Injecting backend URL: $DATA_BOARD_INTERNAL_BACKEND_URL"
 
 # Replace the placeholder in the nginx config
-sed -i "s|DATA_BOARD_INTERNAL_BACKEND_URL|$BACKEND_URL|g" /etc/nginx/conf.d/default.conf
+envsubst '${DATA_BOARD_INTERNAL_BACKEND_URL}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp && \
+mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
+
