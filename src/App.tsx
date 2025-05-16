b3fa5bd5b70d5e729635ce226da276bd13d1dba7
Erik Schwarz <erik.schwarz@psi.ch>
2025-05-16 09:55:22 +0200

    Added healthcheck for the backend, and updated showsnackbar to allow for infinite display, as well as queueing the messages in a stack of up to three messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { CssBaseline } from "@mui/material";
 import DashboardLayout from "./layouts/DashboardLayout";
 import { ApiProvider } from "./components/ApiContext/ApiContext";
+import axios from "axios";
+import showSnackbarAndLog, { logToConsole } from "./helpers/showSnackbar";
 
 interface EnvWindow extends Window {
     _env_?: {
@@ -14,6 +16,39 @@ const App: React.FC = () => {
         backendUrl:
             (window as EnvWindow)._env_?.DATA_BOARD_PUBLIC_BACKEND_URL || "",
     };
+
+    useEffect(() => {
+        const controller = new AbortController();
+
+        const ping = () => {
+            const timeout = setTimeout(() => controller.abort(), 5000);
+
+            axios
+                .get(`${apiUrls.backendUrl}/health`, {
+                    signal: controller.signal,
+                })
+                .then(() =>
+                    logToConsole("Backend seems alive and well", "success")
+                )
+                .catch((err) =>
+                    showSnackbarAndLog(
+                        "Couldn't reach backend",
+                        "error",
+                        err,
+                        -1
+                    )
+                )
+                .finally(() => clearTimeout(timeout));
+        };
+
+        ping();
+        const interval = setInterval(ping, 60000);
+
+        return () => {
+            clearInterval(interval);
+            controller.abort();
+        };
+    }, [apiUrls.backendUrl]);
 
     return (
         <ApiProvider apiUrls={apiUrls}>
